c0cd87c905ebb34411d3925c7fbc5f877190feea
SeojinSeojin <1106laura@naver.com>
2023-09-23 07:26:44 +0900

    오답노트 스타일 적용

--- src/pages/review.jsx
+++ src/pages/review.jsx
@@ -2,7 +2,7 @@ import { Providers } from '@/app/providers';
 import Header from '@/components/Header';
 import LeftNav from '@/components/LeftNav';
 import RightNav from '@/components/RightNav';
-import { Box, Grid, Text } from '@chakra-ui/react';
+import { Box, Flex, Grid, Text } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
@@ -13,22 +13,44 @@ function Review(props) {
   return (
     <Box
       bg={
-        selectedIdx === undefined
-          ? 'white'
-          : selectedIdx + 1 === question.answer
-          ? 'green.50'
-          : 'red.100'
+        selectedIdx !== undefined && selectedIdx + 1 !== question.answer
+          ? 'red.100'
+          : 'white'
       }
+      p='8px 12px'
+      borderRadius='12px'
     >
-      <Text>{question.question}</Text>
+      <Text fontSize='20px' fontWeight='bold' mb='8px'>
+        {question.question}
+      </Text>
       {question.choices.map((c, cIdx) => (
         <Text
+          fontSize='18px'
           key={cIdx}
           border='1px solid'
           borderColor={selectedIdx === cIdx ? 'purple.800' : 'transparent'}
           onClick={() => setSelectedIdx(cIdx)}
+          cursor='pointer'
+          p='4px 8px'
+          borderRadius='8px'
+          bg={
+            selectedIdx !== undefined &&
+            selectedIdx + 1 === question.answer &&
+            selectedIdx === cIdx
+              ? 'green.200'
+              : 'transparent'
+          }
+          _hover={{
+            bg:
+              selectedIdx !== undefined &&
+              selectedIdx + 1 === question.answer &&
+              selectedIdx === cIdx
+                ? 'green.200'
+                : 'rgba(220,220,220,0.5)',
+          }}
+          mt='4px'
         >
-          {cIdx + 1} {c}
+          [{cIdx + 1}] {c}
         </Text>
       ))}
     </Box>
@@ -49,18 +71,18 @@ export default function Home() {
         <Header />
         <Grid gridTemplateColumns='264px auto 264px'>
           <LeftNav />
-          <Box h='calc(100vh - 60px)' overflow='scroll'>
+          <Box h='calc(100vh - 60px)' overflow='scroll' p='28px 20px'>
             {result.length === 0 ? (
               <div>
                 <Text>오답이 없어요!</Text>
                 <Link href='/'>홈으로 가기</Link>
               </div>
             ) : (
-              <div>
+              <Flex flexDirection='column' gap='40px'>
                 {result.map((r, rIdx) => (
                   <Review key={rIdx} question={r} />
                 ))}
-              </div>
+              </Flex>
             )}
           </Box>
           <RightNav />
