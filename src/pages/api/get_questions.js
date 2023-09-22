29874a34aa9bb03fe77b981bae7bb2d11b43b4b8
SeojinSeojin <1106laura@naver.com>
2023-09-23 01:13:57 +0900

    GPT가 만들어준 문제 파싱하기

--- src/pages/api/get_questions.js
+++ src/pages/api/get_questions.js
@@ -1,8 +1,26 @@
 import { getChat } from '@/utils/chat';
 import { getScholarResults } from '@/utils/scholar';
 
+function answerParser(str) {
+  const regex =
+    /\[QuestionIndex\] (\d+)\n\[Question\] (.*?)\n\[Choice-1\] (.*?)\n\[Choice-2\] (.*?)\n\[Choice-3\] (.*?)\n\[Choice-4\] (.*?)\n\[Answer\] (\d+)/g;
+
+  let match;
+  const questions = [];
+
+  while ((match = regex.exec(str)) !== null) {
+    questions.push({
+      questionIndex: parseInt(match[1], 10),
+      question: match[2],
+      choices: [match[3], match[4], match[5], match[6]],
+      answer: parseInt(match[7], 10),
+    });
+  }
+  return questions;
+}
+
 export default async function handler(request, response) {
-  const query = request.query.query;
+  const query = decodeURI(request.query.query);
   const scholarResults = await getScholarResults(query);
   const scholarResultsToText = scholarResults
     .map((sr) => `[${sr.title}]\n${sr.snippet}`)
@@ -12,10 +30,21 @@ export default async function handler(request, response) {
     Below are trend papers from google scholar search engine.\n
     ${scholarResultsToText}
     
-    make 5 vocabulary test by [Multiple Choice Fill-in-the-Blank Example] format.
+    make 5 vocabulary test by [Multiple Choice Fill-in-the-Blank Example] format. Give answer also.
+    The format of test must be like the example below.
+    ------
+    [QuestionIndex] 1
+    [Question] The importance of _______ was highlighted in interviews with welfare clients after welfare reform.
+    [Choice-1] quantitative data
+    [Choice-2] qualitative data
+    [Choice-3] statistical analysis
+    [Choice-4] data mining
+    [Answer] 2
+    ------
     `;
 
   const result = await getChat(systemMessage, [], '');
+  const parsedResult = answerParser(result);
 
-  return response.status(200).json({ result });
+  return response.status(200).json({ result: parsedResult });
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,11 @@
 import { Providers } from '@/app/providers';
-import { Button, Flex, Input, Text } from '@chakra-ui/react';
+import { Button, Flex, Input } from '@chakra-ui/react';
 import axios from 'axios';
 import { useRef, useState } from 'react';
 
 export default function Home() {
   const inputRef = useRef();
-  const [result, setResult] = useState(null);
+  const [result, setResult] = useState([]);
 
   return (
     <Providers>
@@ -16,7 +16,7 @@ export default function Home() {
           color='white'
           bgColor='green.600'
           onClick={() => {
-            const inputValue = inputRef.current.value;
+            const inputValue = encodeURI(inputRef.current.value);
             axios.get(`/api/get_questions?query=${inputValue}`).then((r) => {
               setResult(r.data.result);
             });
@@ -25,7 +25,24 @@ export default function Home() {
           문제 만들기
         </Button>
       </Flex>
-      <Text whiteSpace='pre-wrap'>{result}</Text>
+      <Flex direction='column' gap='4px'>
+        {result.map((r) => (
+          <div key={r.questionIndex}>
+            <Flex gap='8px'>
+              <span>{r.questionIndex}.</span>
+              <span>{r.question}</span>
+            </Flex>
+            <div>
+              {r.choices.map((c, cIdx) => (
+                <div key={cIdx}>
+                  [{cIdx + 1}] {c}
+                </div>
+              ))}
+            </div>
+            <div>(answer: {r.answer})</div>
+          </div>
+        ))}
+      </Flex>
     </Providers>
   );
 }
