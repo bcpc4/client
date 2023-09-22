15db319f91b1ac3c084b341152329c933d541726
SeojinSeojin <1106laura@naver.com>
2023-09-23 06:21:52 +0900

    대화하기 기능 추가 (한 번만 대화 가능)

---
+++ src/pages/api/get_second_conversation.js
@@ -0,0 +1,29 @@
+import { getChat } from '@/utils/chat';
+import { getScholarResults } from '@/utils/scholar';
+
+export default async function handler(request, response) {
+  const query = decodeURI(request.query.query);
+  const prevChatChat = decodeURI(request.query.prevChatChat);
+  const curUserChat = decodeURI(request.query.curUserChat);
+  const scholarResults = await getScholarResults(query);
+  const scholarResultsToText = scholarResults
+    .map((sr) => `[${sr.title}]\n${sr.snippet}`)
+    .join('\n\n');
+
+  const systemMessage = `You are AI graduate student, major in ${query}\n
+    Below are trend papers from google scholar search engine.\n
+    ${scholarResultsToText}
+    
+    You're at a conference, talking with colleagues in the same field. 
+    Consider the topics of the papers presented, say something to bring up as a starting point for conversation. Questioning might be good.
+    It must be less than 100 letter. no quotation marks. 
+    `;
+
+  const result = await getChat(
+    systemMessage,
+    [{ role: 'assistant', content: prevChatChat }],
+    curUserChat
+  );
+
+  return response.status(200).json({ result });
+}
